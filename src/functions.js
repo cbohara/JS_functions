15c98716e88ac504faceca18e3083939106d4849
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-01-21 20:40:40 -0800

    finished

--- src/functions.js
+++ src/functions.js
@@ -63,7 +63,13 @@ var oaklandStreets = {
 // 4. Create a function named functions.imHere that creates and returns a new array. Within the function, iterate through the students array and use the .push() method to add each student to the new array.
 
 // Your code here!
-
+functions.imHere = function(){
+  var array = [];
+  for(var key in students){
+    array.push(students[key]);
+  }
+  return array;
+}
 
 
 
@@ -71,7 +77,14 @@ var oaklandStreets = {
 // 5. Create a function named functions.placesIveBeen that creates and returns a new array. Within the function, iterate through the oaklandStreets array and use the .push() method to add each place (value only) to the new array.
 
 // Your code here!
+functions.placesIveBeen = function(){
+  var array = [];
+  for(var key in oaklandStreets){
+    array.push(oaklandStreets[key]);
+  }
+  return array;
 
+}
 
 
 
@@ -81,41 +94,44 @@ var newStudents = students.slice();
 // 6. Create a function named functions.findLevio that iterates through the newStudents array and checks if the current student is levio. If so, return levio. If the current student is not levio, use the .push() method to add another student to the newStudents array.
 
 // Your code here!
-
-
-
-
+functions.findLevio = function(){
+  for(var i = 0; i< newStudents.length; i++){
+    if(newStudents[i] === 'levio')
+      return'levio';
+    else
+      newStudents.push(newStudents[i]);
+    }
+  }
 
 // 7. Create a function named functions.dudeWheresMyCar that returns the location of my car. The function should take in a name and a location, and it should return a string that concatenates the arguments together.
 
 //Ex: functions.dudeWheresMyCar('Kristina', 'New Zealand') // 'Kristina left the car in New Zealand'
 
 // Your code here!
-
-
-
-
+functions.dudeWheresMyCar = function(name, location){
+  return name + " left the car in " + location;
+}
 
 // 8. Invoke the functions.dudeWheresMyCar function and pass in Shanna and California as the arguments.
 functions.invokeDudeWheresMyCar = function() {
   // Your code here!
-
+  return functions.dudeWheresMyCar('Shanna', 'California');
 }
 
 // 9. Invoke the functions.modulo function so that it returns the remainder when 15 is divided by 4
 functions.invokeModulo = function() {
   // Your code here!
-  
+  return functions.modulo(15,4);
 }
 // 10. Invoke the functions.multiply so that it returns the value when multiplying 25 and 43
 functions.invokeMultiply = function() {
    // Your code here!
-  
+  return functions.multiply(25,43);
 }
 // 11. Invoke the functions.subtract so that it returns the value when subtracting 72 from 238
 functions.invokeSubtract = function() {
    // Your code here!
-  
+  return functions.subtract(238,72);
 }
 
 }());
